53c3adf35da19532395e3ec5adc0fc2ad20833ff
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-05-18 09:12:20 -0300

    DTO

--- backend/src/main/java/io/github/guisofiati/dscatalog/services/CategoryService.java
+++ backend/src/main/java/io/github/guisofiati/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package io.github.guisofiati.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import io.github.guisofiati.dscatalog.dto.CategoryDTO;
 import io.github.guisofiati.dscatalog.entities.Category;
 import io.github.guisofiati.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,11 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list
+				.stream()
+				.map(x -> new CategoryDTO(x))
+				.collect(Collectors.toList());
 	}
 }
